ad5cbafc8d2e046411a8da92f2562a9627c9e93b
Miguel Acosta <miguel86acosta@gmail.com>
2023-02-03 00:22:55 +0100

    creando xml generador de objetos

--- src/main/java/IoC/UsoEmpleados.java
+++ src/main/java/IoC/UsoEmpleados.java
@@ -4,11 +4,13 @@ public class UsoEmpleados {
 
 	public static void main(String[] args) {
 		//Creación de objetos tipo empleado
-
+/*
 		Empleados empleado1 = new SecretarioEmpleado();
 
 		//Uso de los objetos creados
 
-		System.out.println(empleado1.getTareas());
+		System.xout.println(empleado1.getTareas());*/
+
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
 	}
 }
